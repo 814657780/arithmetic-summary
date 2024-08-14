969a70b0f1919170a24dbdf978aeaf64e57ec0f3
814657780 <18037818812@163.com>
2024-08-14 23:25:44 +0800

    算法之前K个高频元素

---
+++ 算法实例/33、前K个高频元素/347、前k个高频元素.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function (nums, k) {
+    // 1、先声明一个map，用来存储相同的整数个数
+    // 2、遍历nums，然后比较相同的元素，然后添加到对应的数组中
+    // 3、然后再把map转为数组，再进行排序，再进行截取，再进行抽取数组中的第一项组成新的数组
+    let [map, len] = [new Map(), nums.length]
+    for (let i = 0; i < len; i++) {
+        let num = nums[i]
+        if (map.has(num)) {
+            map.get(num)++
+        } else {
+            map.set(num, 1)
+        }
+    }
+    let arr = [...map.entries].sort((a, b) => a[1] - b[1]).slice(0, k).map(item => item[0])
+    return arr;
+};
