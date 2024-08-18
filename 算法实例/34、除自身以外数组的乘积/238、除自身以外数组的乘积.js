e9818ea8f65fd275df9a39d016e5d2cdd543f2f0
814657780 <18037818812@163.com>
2024-08-18 21:40:09 +0800

    算法之除自身以外数组的乘积

---
+++ 算法实例/34、除自身以外数组的乘积/238、除自身以外数组的乘积.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+    // 第一步先声明一个新数组
+    // 第二步声明一个左侧累积乘积
+    // 第三步从左到右遍历，然后更新左侧累积乘积
+    // 第四步声明一个右侧累积乘积
+    // 第五步从右到左遍历，然后更新右侧累积乘积
+    // 第六步返回新数组
+    const [n, answer] = [nums.length, new Array(n)];
+    // 初始化左侧累积乘积
+    let leftProduct = 1;
+    for (let i = 0; i < n; i++) {
+        answer[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    // 计算右侧累积乘积并更新 answer
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return answer;
+};
